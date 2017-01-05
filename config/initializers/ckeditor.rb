7adb39d3f369ad09bef5032099373d70af76296c
Guillaume Kaisin <gkaisin@auctelia.com>
2017-01-05 12:53:56 +0100

    precompile shit

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -6,6 +6,8 @@ Ckeditor.setup do |config|
   # available as additional gems.
   require 'ckeditor/orm/active_record'
 
+  assets_languages =  [ 'fr', 'en']
+
   # Allowed image file types for upload.
   # Set to nil or [] (empty array) for all file types
   # By default: %w(jpg jpeg png gif tiff)
