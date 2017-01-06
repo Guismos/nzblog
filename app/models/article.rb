8de0d8811f36ca1ae264c4c06d034ab3179c78dd
Guillaume Kaisin <gkaisin@auctelia.com>
2017-01-06 13:26:08 +0100

    size image

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_attached_file :picture, default_url: "/images/missing.png", :styles => { :medium =>     "300x300#", :thumb => "200x200#" }
+  has_attached_file :picture, default_url: "/images/missing.png", :styles => { :large => "1920x1080", :medium =>     "300x300#", :thumb => "200x200#" }
 	validates_attachment :picture, content_type: { content_type:     ["image/jpg", "image/jpeg", "image/png"] }
 
   has_many :comments
